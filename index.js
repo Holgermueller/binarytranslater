fb9af3aa0dac37b3d8bb4d36f37a65e3813c618c
holgermueller <9jholger@gmail.com>
2022-11-07 18:38:19 -0600

    got form values to log

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const submitForm = () => {
   console.log("click");
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let agree = document.getElementById("agree").checked;
+  console.log(name, email, agree);
+
+  resetForm();
+};
+
+const resetForm = () => {
+  let form = document.getElementById("form");
+  form.reset();
 };
